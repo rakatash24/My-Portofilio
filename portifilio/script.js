1763e237edafd1f817de570722fd44b667d925d1
rakatash24 <rakatash24@gmail.com>
2024-12-07 22:43:00 +0400

    Add files via upload

---
+++ portifilio/script.js
@@ -0,0 +1,10 @@
+// Smooth Scroll for navigation links
+document.querySelectorAll('.nav-links a').forEach(anchor => {
+    anchor.addEventListener('click', function (e) {
+        e.preventDefault();
+        const targetId = this.getAttribute('href').substring(1);
+        document.getElementById(targetId).scrollIntoView({
+            behavior: 'smooth'
+        });
+    });
+});
